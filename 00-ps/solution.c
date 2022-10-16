ae0eb4db46bf934c1df598b7084300f2153d9fc3
7aa7oo7aa7 <proniakin.aa@phystech.edu>
2022-10-16 19:57:18 +0300

    changed calloc to malloc

--- 00-ps/solution.c
+++ 00-ps/solution.c
@@ -32,8 +32,8 @@ void ps(void)
 	char** argv = (char**) malloc(ARG_MAX * sizeof(char*));
     char** envp = (char**) malloc(ARG_MAX * sizeof(char*));
     for (int i = 0; i < ARG_MAX; ++i) {
-        argv[i] = (char*) calloc(MAX_ARG_STRLEN, sizeof(char));
-        envp[i] = (char*) calloc(MAX_ARG_STRLEN, sizeof(char));
+        argv[i] = (char*) malloc(MAX_ARG_STRLEN * sizeof(char));
+        envp[i] = (char*) malloc(MAX_ARG_STRLEN * sizeof(char));
     }
 
 	for (struct dirent* proc_dirent = readdir(proc_dir); proc_dirent != NULL; proc_dirent = readdir(proc_dir)) {
