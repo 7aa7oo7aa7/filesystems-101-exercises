9a39fc8b5ba1aeccf2d0e19ea76b82475471d34c
7aa7oo7aa7 <proniakin.aa@phystech.edu>
2022-11-06 19:16:27 +0300

    added left_to_read

--- 05-ext2-read-dir/solution.c
+++ 05-ext2-read-dir/solution.c
@@ -16,17 +16,16 @@ struct ext2_dir_entry_2* get_dir_entry(void* buf, off_t offset) {
     return (struct ext2_dir_entry_2*) (buf + offset);
 }
 
-int report_direct(int img, const uint32_t block, const size_t block_size, void* buf) {
+int report_direct(int img, const uint32_t block, const size_t block_size, ssize_t* left_to_read, void* buf) {
     ssize_t bytes_read = read_block(img, block, block_size, buf);
     if (bytes_read < (ssize_t) block_size) {
         return -errno;
     }
 
     char* file_name = (char*) calloc(EXT2_NAME_LEN + 1, sizeof(char));
-    ssize_t left_to_read = (ssize_t) block_size;
     struct ext2_dir_entry_2* dir_entry = NULL;
     off_t offset = 0;
-    for (dir_entry = get_dir_entry(buf, 0); dir_entry->inode != 0 && left_to_read > 0; dir_entry = get_dir_entry(buf, offset)) {
+    for (dir_entry = get_dir_entry(buf, 0); dir_entry->inode != 0 && *left_to_read > 0; dir_entry = get_dir_entry(buf, offset)) {
         memcpy(file_name, dir_entry->name, dir_entry->name_len);
         file_name[dir_entry->name_len] = '\0';
         if (dir_entry->file_type != EXT2_FT_REG_FILE || dir_entry->file_type != EXT2_FT_DIR) {
@@ -34,7 +33,7 @@ int report_direct(int img, const uint32_t block, const size_t block_size, void* 
         }
         char file_type = (dir_entry->file_type == EXT2_FT_REG_FILE) ? 'f' : 'd';
         report_file(dir_entry->inode, file_type, file_name);
-        left_to_read -= dir_entry->rec_len;
+        *left_to_read -= dir_entry->rec_len;
         offset += dir_entry->rec_len;
     }
 
@@ -42,18 +41,18 @@ int report_direct(int img, const uint32_t block, const size_t block_size, void* 
     return 0;
 }
 
-int report_indirect(int img, const uint32_t block, const size_t block_size, uint32_t* buf, int block_type) {
+int report_indirect(int img, const uint32_t block, const size_t block_size, ssize_t* left_to_read, uint32_t* buf, int block_type) {
     ssize_t bytes_read = read_block(img, block, block_size, buf);
     if (bytes_read < (ssize_t) block_size) {
         return -errno;
     }
     void* indirect_block_buf = calloc(block_size, sizeof(char));
     int retval = 0;
-    for (size_t i = 0; i < block_size / sizeof(uint32_t) && buf[i] != 0; ++i) {
+    for (size_t i = 0; i < block_size / sizeof(uint32_t) && buf[i] != 0 && *left_to_read > 0; ++i) {
         if (block_type == EXT2_IND_BLOCK) {
-            retval = report_direct(img, buf[i], block_size, indirect_block_buf);
+            retval = report_direct(img, buf[i], block_size, left_to_read, indirect_block_buf);
         } else {
-            retval = report_indirect(img, buf[i], block_size, (uint32_t*) indirect_block_buf, i - 1);
+            retval = report_indirect(img, buf[i], block_size, left_to_read, (uint32_t*) indirect_block_buf, i - 1);
         }
         if (retval < 0) {
             break;
@@ -95,13 +94,14 @@ int dump_dir(int img, int inode_nr) {
     }
 
     // read all blocks
+    ssize_t left_to_read = inode.i_size;
     void* buf = calloc(block_size, sizeof(char));
     int retval = 0;
-    for (size_t i = 0; i < EXT2_N_BLOCKS && inode.i_block[i] != 0; ++i) {
+    for (size_t i = 0; i < EXT2_N_BLOCKS && inode.i_block[i] != 0 && left_to_read > 0; ++i) {
         if (i < EXT2_NDIR_BLOCKS) {
-            retval = report_direct(img, inode.i_block[i], block_size, buf);
+            retval = report_direct(img, inode.i_block[i], block_size, &left_to_read, buf);
         } else if (i == EXT2_IND_BLOCK || i == EXT2_DIND_BLOCK || i == EXT2_TIND_BLOCK) {
-            retval = report_indirect(img, inode.i_block[i], block_size, (uint32_t*) buf, i);
+            retval = report_indirect(img, inode.i_block[i], block_size, &left_to_read, (uint32_t*) buf, i);
         } else {
             retval = -1;
         }
