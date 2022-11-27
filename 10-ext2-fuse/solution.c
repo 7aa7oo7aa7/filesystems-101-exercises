3e4eb91a09cb143ed6bc3b043a9301964a781c5c
7aa7oo7aa7 <proniakin.aa@phystech.edu>
2022-11-27 23:29:07 +0300

    try assert 6

--- 10-ext2-fuse/solution.c
+++ 10-ext2-fuse/solution.c
@@ -129,7 +129,6 @@ int get_inode(int img, const char* path, struct ext2_super_block* super_block) {
         }
 
         if ((inode.i_mode & LINUX_S_IFDIR) == 0) {
-            assert(0);
             return -ENOTDIR;
         }
 
@@ -142,6 +141,7 @@ int get_inode(int img, const char* path, struct ext2_super_block* super_block) {
 
         inode_nr = get_next_inode(img, block_size, &inode, filename, filename_len);
         if (inode_nr < 0) {
+            assert(0);
             return inode_nr;
         } else if (inode_nr == 0) {
             return -ENOENT;
